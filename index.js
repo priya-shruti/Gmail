c44ff5dd6e80055dd255c9fa0a1cc3ee976da935
priya-shruti <priya.shruti11@gmail.com>
2023-12-12 00:10:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+function sendMail() {
+    var params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+    };
+    const serviceID = "service_v1g2mxh";
+    const templateID = "template_dj36t53";
+    emailjs.send(serviceID, templateID, params)
+        .then(
+            res => {
+                document.getElementById("name").value = "";
+                document.getElementById("email").value = "";
+                document.getElementById("message").value = "";
+                console.log(res);
+                alert("Your message sent successfully")
+            }
+        )
+        .catch((err) => console.log(err));
+}
